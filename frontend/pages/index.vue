f8943a39dcd7ff6b0cb13a904005f63d2d55005b
samuel.butta <samuel.butta@oracle.com>
2024-08-04 22:03:07 +0200

    Update UI

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,49 +1,56 @@
 <script setup lang="ts">
 import type {Ref} from "vue";
 
-const route = useRoute()
-
 const word: Ref<string> = ref("")
 const fromLang: Ref<string> = ref("EN")
 const toLang: Ref<string> = ref("CS")
 const translations: Ref<Array<string> | null> = ref(null)
-const pending : Ref<boolean> = ref(false)
+const pending: Ref<boolean> = ref(false)
+
+interface TranslateResponse {
+  translations: string[];
+}
 
 async function submitForm(): Promise<void> {
   translations.value = null;
   pending.value = true
-  const response = await $fetch("http://localhost:8080/translator/translate", {
-    method: "POST",
-    body: {
-      word: word.value,
-      fromLang: fromLang.value,
-      toLang: toLang.value,
-    }
-  })
-  console.log(response)
-  translations.value = response["translations"]
-  pending.value = false
-}
+  try {
+    const response: TranslateResponse = await $fetch("http://localhost:8080/translator/translate", {
+      method: "POST",
+      body: {
+        word: word.value,
+        fromLang: fromLang.value,
+        toLang: toLang.value,
+      }
+    })
+    console.log(response)
+    translations.value = response.translations
+  } catch (error) {
+    console.log(error)
+  } finally {
+    pending.value = false
 
+  }
+}
 </script>
 
 <template>
   <div>
-    <section class="bg-blue-50">
-      <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+    <section class="bg-gray-100">
+      <div class="flex flex-col items-center justify-center mx-auto h-screen">
         <a href="#" class="flex items-center mb-6 text-2xl text-gray-900">
           Dictionary AI
         </a>
 
-        <div>
+        <div class="w-full md:w-1/2 lg:w-1/4 p-10 md:p-0">
           <form @submit.prevent="submitForm">
             <div>
               <label for="word" class="block mb-2 text-sm">Input</label>
               <input v-model="word" type="text" name="word" id="word"
                      class="bg-gray-50 border border-gray-300 text-gray-900 block w-full p-2.5" required="">
             </div>
-            <div class="flex space-x-5">
-              <div>
+            <div class="flex space-x-5 w-full">
+              <div class="w-full">
                 <label for="from-lang" class="block mb-2 text-sm">From</label>
                 <select v-model="fromLang" id="from-lang"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm block w-full p-2.5">
@@ -51,7 +58,7 @@ async function submitForm(): Promise<void> {
                   <option value="CS">Czech</option>
                 </select>
               </div>
-              <div>
+              <div class="w-full">
                 <label for="to-lang" class="block mb-2 text-sm">To</label>
                 <select v-model="toLang" id="to-lang"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm block w-full p-2.5">
@@ -62,24 +69,45 @@ async function submitForm(): Promise<void> {
             </div>
             <div class="mt-2">
               <input type="submit" value="Translate"
-                     class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none w-full"/>
+                     class="text-white bg-blue-700 hover:bg-blue-800 font-medium text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none w-full"/>
             </div>
           </form>
-        </div>
 
-        <div>
-          <div role="status" v-if="pending">
-            <svg aria-hidden="true" class="w-8 h-8 text-gray-200 animate-spin fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
-              <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
-            </svg>
-            <span class="sr-only">Loading...</span>
+          <div class="flex flex-col items-center justify-center mx-auto">
+            <div role="status" v-if="pending">
+              <svg aria-hidden="true" class="w-8 h-8 text-gray-200 animate-spin fill-blue-600" viewBox="0 0 100 101"
+                   fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                    fill="currentColor"/>
+                <path
+                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                    fill="currentFill"/>
+              </svg>
+              <span class="sr-only">Loading...</span>
+            </div>
+
+            <ul class="list-decimal text-lg">
+              <li v-for="translation in translations">{{ translation }}</li>
+            </ul>
+
+            <div v-if="translations != null && translations.length == 0"
+                 class="
+                 w-full flex items-center p-4 mb-4 text-sm text-yellow-800 border border-yellow-300 bg-yellow-50"
+                 role="alert">
+              <svg class="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                   fill="currentColor" viewBox="0 0 20 20">
+                <path
+                    d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+              </svg>
+              <span class="sr-only">Info</span>
+              <div>
+                <span class="font-medium">Word not found!</span>
+              </div>
+            </div>
           </div>
-
-          <pre v-if="translations">{{ translations }}</pre>
         </div>
       </div>
-
     </section>
   </div>
 </template>
