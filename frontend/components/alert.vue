94a464d109bcacb4c9adb935298227ee096cd7d1
samuel.butta <samuel.butta@oracle.com>
2024-08-05 21:23:04 +0200

    Add spinner and alert components, add icons plugin

---
+++ frontend/components/alert.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+
+interface Props {
+  show: boolean,
+  message: string,
+  type: 'info' | 'warning' | 'error' | 'success'
+}
+
+defineProps<Props>();
+
+const icons = {
+  success: "ep:success-filled",
+  info: "ic:baseline-info",
+  error: "material-symbols:amp-error",
+  warning: "material-symbols:warning",
+}
+
+const classes = {
+  info: 'text-blue-800 border-blue-300 bg-blue-50',
+  success: 'text-green-800 border-green-300 bg-green-50',
+  warning: 'text-yellow-800 border-yellow-300 bg-yellow-50',
+  error: 'text-red-800 border-red-300 bg-red-50',
+}
+
+</script>
+
+<template>
+  <div v-if="show"
+       :class="['w-full flex items-center p-4 mb-4 text-sm border', classes[type]]"
+       role="alert">
+    <Icon :name="icons[type]" class="mr-1"/>
+    <span class="sr-only">Info</span>
+    <div>
+      <span class="font-medium">{{ message }}</span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,19 +1,26 @@
 <script setup lang="ts">
 import type {Ref} from "vue";
+import Spinner from "~/components/spinner.vue";
 
-const word: Ref<string> = ref("")
-const fromLang: Ref<string> = ref("EN")
-const toLang: Ref<string> = ref("CS")
-const translations: Ref<Array<string> | null> = ref(null)
-const pending: Ref<boolean> = ref(false)
+enum Language {
+  EN = 'EN',
+  CS = 'CS'
+}
 
 interface TranslateResponse {
   translations: string[];
 }
 
+const word: Ref<string> = ref("");
+const fromLang: Ref<Language> = ref(Language.EN);
+const toLang: Ref<Language> = ref(Language.CS);
+const translations: Ref<Array<string> | null> = ref(null);
+const pending: Ref<boolean> = ref(false);
+const showError: Ref<boolean> = ref(false);
+
 async function submitForm(): Promise<void> {
   translations.value = null;
-  pending.value = true
+  pending.value = true;
   try {
     const response: TranslateResponse = await $fetch("http://localhost:8080/translator/translate", {
       method: "POST",
@@ -23,13 +30,13 @@ async function submitForm(): Promise<void> {
         toLang: toLang.value,
       }
     })
-    console.log(response)
-    translations.value = response.translations
+    console.log(response);
+    translations.value = response.translations;
   } catch (error) {
-    console.log(error)
+    console.log(error);
+    showError.value = true;
   } finally {
-    pending.value = false
-
+    pending.value = false;
   }
 }
 </script>
@@ -39,6 +46,7 @@ async function submitForm(): Promise<void> {
     <section class="bg-gray-100">
       <div class="flex flex-col items-center justify-center mx-auto h-screen">
         <a href="#" class="flex items-center mb-6 text-2xl text-gray-900">
+          <Icon name="material-symbols:dictionary-outline-rounded" class="mr-1"/>
           Dictionary AI
         </a>
 
@@ -74,37 +82,16 @@ async function submitForm(): Promise<void> {
           </form>
 
           <div class="flex flex-col items-center justify-center mx-auto">
-            <div role="status" v-if="pending">
-              <svg aria-hidden="true" class="w-8 h-8 text-gray-200 animate-spin fill-blue-600" viewBox="0 0 100 101"
-                   fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path
-                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                    fill="currentColor"/>
-                <path
-                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                    fill="currentFill"/>
-              </svg>
-              <span class="sr-only">Loading...</span>
-            </div>
+
+            <Spinner :show="pending"/>
 
             <ul class="list-decimal text-lg">
               <li v-for="translation in translations">{{ translation }}</li>
             </ul>
 
-            <div v-if="translations != null && translations.length == 0"
-                 class="
-                 w-full flex items-center p-4 mb-4 text-sm text-yellow-800 border border-yellow-300 bg-yellow-50"
-                 role="alert">
-              <svg class="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                   fill="currentColor" viewBox="0 0 20 20">
-                <path
-                    d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
-              </svg>
-              <span class="sr-only">Info</span>
-              <div>
-                <span class="font-medium">Word not found!</span>
-              </div>
-            </div>
+            <Alert :show="showError" message="Something went wrong." type="error"/>
+            <Alert :show="translations != null && translations.length == 0" message="Word not found!" type="warning"/>
+
           </div>
         </div>
       </div>
