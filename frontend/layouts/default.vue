d5c9e2198cb0e2c09b014a09fa1585803d4d6685
samuel.butta <samuel.butta@oracle.com>
2024-09-12 21:33:36 +0200

    Improve section position

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -4,8 +4,8 @@
   <div class="min-h-screen flex flex-col">
     <AppHeader />
     <div class="flex-grow">
-      <section class="bg-gray-100 pt-6 ms:pt-0">
-        <div class="flex flex-col items-center justify-center mx-auto min-h-screen">
+      <section class="bg-gray-100">
+        <div class="flex flex-col items-center sm:justify-center mx-auto min-h-screen pb-24">
           <slot />
         </div>
       </section>
